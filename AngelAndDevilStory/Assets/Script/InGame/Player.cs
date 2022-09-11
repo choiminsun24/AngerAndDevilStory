ef404a165b7b0cfef13f9847c55e4e7183f44267
choiminsun <backwonand@sookmyung.ac.kr>
2022-09-12 03:15:39 +0900

    Make Button Animation

--- AngelAndDevilStory/Assets/Script/InGame/Player.cs
+++ AngelAndDevilStory/Assets/Script/InGame/Player.cs
@@ -7,17 +7,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private bool[] AandD; //[0]는 천사가 눌렸는지 여부, [1]은 악마가 눌렸는지 여부를 나타냄.
+    private Animator[] animAD;
+
+    public GameObject buttonA, buttonD;
+
     static private int helpness = 0; 
 
     // Start is called before the first frame update
     void Start()
     {
         AandD = new bool[2];
+        animAD = new Animator[2];
+
         //천사 악마 bool 디폴트로 세팅
         AandD[0] = false; //천사
         AandD[1] = false; //악마
 
-        Debug.Log(!true);
+        animAD[0] = buttonA.GetComponent<Animator>();
+        animAD[1] = buttonD.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -52,9 +59,10 @@ public class Player : MonoBehaviour
     public void P_Status(int btn) //천사 또는 악마 버튼 눌렀을 때
     {
         AandD[btn] = !AandD[btn];
-
-        Debug.Log(AandD[btn]);
-
+        Debug.Log("눌렸다");
         helpness = change_helpness();
+        Debug.Log("바뀌었다");
+        animAD[btn].SetBool("clicked", AandD[btn]);
+        Debug.Log(AandD[btn]);
     }
 }
